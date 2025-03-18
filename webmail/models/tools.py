8127cde76bbf781a7eb0cc18479566a0d490fe65
Sylvain LE GAL <sylvain.legal@grap.coop>
2025-03-18 02:20:51 +0100

    REF remove useless code

--- webmail/models/tools.py
+++ webmail/models/tools.py
@@ -3,36 +3,9 @@
 import re
 
 
-def modified_base64(s: str) -> str:
-    s_utf7 = s.encode("utf-7")  #
-    aaa = s_utf7[1:-1].decode().replace("/", ",")  # rfc2060
-    return aaa
-
-
 def modified_unbase64(s: str) -> str:
     s_utf7 = "+" + s.replace(",", "/") + "-"
     return s_utf7.encode().decode("utf-7")
-
-
-def encode_imap4_utf7(s: str, errors=None) -> tuple[str, int]:
-    r = list()
-    _in = list()
-    for c in s:
-        if ord(c) in range(0x20, 0x25) or ord(c) in range(0x27, 0x7E):
-            if _in:
-                r.extend(["&", modified_base64("".join(_in)), "-"])
-                del _in[:]
-            r.append(str(c))
-        elif ord(c) == 0x26:
-            if _in:
-                r.extend(["&", modified_base64("".join(_in)), "-"])
-                del _in[:]
-            r.append("&-")
-        else:
-            _in.append(c)
-    if _in:
-        r.extend(["&", modified_base64("".join(_in)), "-"])
-    return "".join(r), len(s)
 
 
 def decode_imap4_utf7(s: str) -> str:
